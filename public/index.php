639653fa302e50b2dcb36e31cf11f0c41e6fb385
zhao <905518706.com>
2020-05-29 11:55:59 +0800

    z

--- public/index.php
+++ public/index.php
@@ -10,19 +10,19 @@
 | is ready to receive HTTP / Console requests from the environment.
 |
 */
-
-$app = require __DIR__.'/../bootstrap/app.php';
-
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request
-| through the kernel, and send the associated response back to
-| the client's browser allowing them to enjoy the creative
-| and wonderful application we have prepared for them.
-|
-*/
-
-$app->run();
+phpinfo();
+//$app = require __DIR__.'/../bootstrap/app.php';
+//
+///*
+//|--------------------------------------------------------------------------
+//| Run The Application
+//|--------------------------------------------------------------------------
+//|
+//| Once we have the application, we can handle the incoming request
+//| through the kernel, and send the associated response back to
+//| the client's browser allowing them to enjoy the creative
+//| and wonderful application we have prepared for them.
+//|
+//*/
+//
+//$app->run();
